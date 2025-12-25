33981a3ba0829b2d6806b9c55dd42182ffe978b5
yadavalu <aadith.ygalu@gmail.com>
2025-12-25 12:50:53 +0100

    Fixed deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',      // Static export
+  images: {
+    unoptimized: true,   // GitHub Pages doesn't support the Next.js Image Optimization API
+  },
+  basePath: '/SafeRail-AI',
+  assetPrefix: '/SafeRail-AI',
 };
 
 export default nextConfig;
